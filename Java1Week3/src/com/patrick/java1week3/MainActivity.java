749ccfd2c907193017658249713c83ac2d1a1881
PatrickAdams <patrickadams@fullsail.edu>
2013-08-20 23:51:39 -0400

    Second commit for Week 3

--- Java1Week3/src/com/patrick/java1week3/MainActivity.java
+++ Java1Week3/src/com/patrick/java1week3/MainActivity.java
@@ -1,7 +1,15 @@
 package com.patrick.java1week3;
 
-import com.patrick.lib.WebConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.patrick.lib.FileStuff;
+import com.patrick.lib.WebConnection;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -10,6 +18,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
@@ -20,6 +29,7 @@ public class MainActivity extends Activity {
 	PostDisplay posts;
 	FavoritePosts favorites;
 	Boolean connected = false;
+	HashMap<String, String> history;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,7 @@ public class MainActivity extends Activity {
 		
 		context = this;
 		ll = new LinearLayout(this);
+		history = new HashMap<String, String>();
 		
 		search = new SearchForm(context, "Enter Game Name", "GO");
 		
@@ -36,8 +47,8 @@ public class MainActivity extends Activity {
 		searchButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
-			public void onClick(View arg0) {
-				Log.i("Click Handler", search.getField().getText().toString());
+			public void onClick(View v) {
+				getPost(search.getField().getText().toString());
 			}
 		});
 		
@@ -68,6 +79,57 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void getPost(String tag) {
+		String baseURL = "http://api.tumblr.com/v2/blog/www.theappmob.com/posts?api_key=zoxEpyAIr35TvVt6P6XYGrN5WiENeOrkqP8sECu5XvuIzR5cRP&tag=";
+		String baseURLWithTag = baseURL + tag;
+		
+		URL finalURL;
+		try{
+			finalURL = new URL(baseURLWithTag);
+			PostRequest pr = new PostRequest();
+			pr.execute(finalURL);
+		} catch (MalformedURLException e) {
+			Log.e("BAD URL", "BLAH BLAH");
+			finalURL = null;
+		}
+	}
+	
+	private class PostRequest extends AsyncTask<URL, Void, String> {
+		
+		@Override
+		protected String doInBackground(URL... urls) {
+			String response = "";
+			for(URL url: urls) {
+				response = WebConnection.getURLStringResponse(url);
+			}
+			
+			return response;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			Log.i("URL RESPONSE", result);
+			try{
+				JSONObject json = new JSONObject(result);
+				JSONObject results = json.getJSONObject("response").getJSONArray("posts").getJSONObject(0);
+				
+				//TODO fix the next line
+				if(results.getString("title").compareTo("N/A")==0){
+					Toast toast = Toast.makeText(context, "No results", Toast.LENGTH_SHORT);
+					toast.show();
+				} else {
+					Toast toast = Toast.makeText(context, "Valid search: " + results.getString("title"), Toast.LENGTH_SHORT);
+					toast.show();
+					history.put(results.getString("title"), results.toString());
+					FileStuff.storeObjectFile(context, "history", history, false);
+					FileStuff.storeStringFile(context, "temp", results.toString(), true);
+				}
+			} catch (JSONException e) {
+				Log.e("JSON", e.getLocalizedMessage());
+			}
+		}
 	}
 
 }
--- Java1Week3/src/com/patrick/lib/WebConnection.java
+++ Java1Week3/src/com/patrick/lib/WebConnection.java
@@ -1,8 +1,13 @@
 package com.patrick.lib;
 
+import java.io.BufferedInputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 public class WebConnection {
 	
@@ -22,12 +27,36 @@ public class WebConnection {
 	private static void netInfo(Context context) {
 		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo ni = cm.getActiveNetworkInfo();
-		if(ni!= null) {
+		if(ni != null) {
 			if(ni.isConnected()) {
 				connectionType = ni.getTypeName();
 				connection = true;
 			}
 		}
+	}
+	
+	public static String getURLStringResponse(URL url) {
+		String response = "";
+		
+		try{
+			URLConnection conn = url.openConnection();
+			BufferedInputStream bin = new BufferedInputStream(conn.getInputStream());
+			
+			byte[] contentBytes = new byte[1024];
+			int bytesRead = 0;
+			StringBuffer responseBuffer = new StringBuffer();
+			
+			while((bytesRead = bin.read(contentBytes)) != -1) {
+				response = new String(contentBytes, 0, bytesRead);
+				responseBuffer.append(response);
+			}
+			
+			return responseBuffer.toString();
+		} catch (Exception e) {
+			Log.e("URL RESPONSE ERROR", e.getLocalizedMessage());
+		}
+		
+		return response;
 	}
 
 }
